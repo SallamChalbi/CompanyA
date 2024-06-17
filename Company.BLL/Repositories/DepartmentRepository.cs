426c0eedcfa765585c0c557b69bedf62829f6e00
SallamChalbi <sallamchalbi@gmail.com>
2024-06-17 18:44:41 +0100

    BLL for Department Repository

---
+++ Company.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Company.BLL.Interfaces;
+using Company.DAL.Data;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext) 
+        { 
+            _dbContext = dbContext;
+        }
+
+        public int Add(Department department)
+        {
+            _dbContext.Departments.Add(department);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Department department)
+        {
+            _dbContext.Departments.Update(department);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department department)
+        {
+            _dbContext.Departments.Remove(department);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int id)
+            => _dbContext.Departments.Find(id);
+
+        public IEnumerable<Department> GetAll()
+            => _dbContext.Departments.AsNoTracking().ToList();
+    }
+}
