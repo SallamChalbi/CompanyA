cc69b9e396b3377f10a2ac7490374064bd04f438
SallamChalbi <sallamchalbi@gmail.com>
2024-06-18 19:29:28 +0100

    PL for Department Controller (Create Page)

--- Company.PL/Controllers/DepartmentController.cs
+++ Company.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Company.BLL.Interfaces;
 using Company.BLL.Repositories;
+using Company.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
@@ -11,9 +12,27 @@ namespace Company.PL.Controllers
         {
             _repository = repository;
         }
+
         public IActionResult Index()
         {
             var department = _repository.GetAll();
+            return View(department);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if(ModelState.IsValid)
+            {
+                var count = _repository.Add(department);
+                if (count > 0)
+                    return RedirectToAction(nameof(Index));
+            }
             return View(department);
         }
     }
